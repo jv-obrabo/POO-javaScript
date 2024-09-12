1e4a9703df78af05f2efb462216f101ac50cbd34
Joao Victor <jv2404200717@gmail.com>
2024-09-12 08:41:03 -0400

    add atributos POO

---
+++ 02-atributo.js
@@ -0,0 +1,9 @@
+class Pessoa {
+    construtor(nome,idade,altura,peso) {// atributos 
+        // inicializaçao dos  atributos
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+}
