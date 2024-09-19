201477c81ea0a1cca4c8adb40eb200425402deb8
joaovictor <jv2404200717@gmail.com>
2024-09-19 07:40:20 -0400

    add heranca poo

---
+++ 07-heranca.js
@@ -0,0 +1,9 @@
+ // classe base pessoa 
+ class Pessoa {
+  constructor(nome, idade,altura,peso) {
+    this.nome = nome;
+    this.idade = idade;
+    this.altura = altura;
+    this.peso = peso;
+  }
+}
