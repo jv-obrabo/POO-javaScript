4d95a7ec001c0e2082e0f4bdb21876e04668369b
João Victor <jv2404200717@gmail.com>
2024-10-01 08:19:15 -0400

    15-encapsulamento-getters-setters.js

---
+++ 15-encapsulamento-getters-setters.js
@@ -0,0 +1,40 @@
+class Personagem {
+  #nome;
+  #classe;
+  #nivel;
+  #vida;
+  #mana;
+
+  construtor(nome, classe, nivel, vida, mana) {
+    this.nome = nome;
+    this.classe = classe;
+    this.nivel = nivel;
+    this.vida = vida;
+    this.mana = mana;
+  }
+  get nome(){
+    return this.#nome; 
+}
+set nome(nome) {
+    this.#nome=nome;
+}
+get classe() { 
+    return this.#classe;
+}
+set classe(classe) {
+    this.#classe=classe;
+}
+
+get nivel() { 
+    return this.#nivel;
+}
+
+get vida() { 
+    return this.#vida;
+}
+
+get mana() { 
+    return this.#mana;
+}
+
+}
