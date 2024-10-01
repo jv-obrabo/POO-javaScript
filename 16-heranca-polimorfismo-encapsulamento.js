3a6e38be060d3a49f92ce30e7f90f71661e0ef2d
João Victor <jv2404200717@gmail.com>
2024-10-01 08:39:10 -0400

    16-heranca-polimorfismo-encapsulamento.js

--- 16-heranca-polimorfismo-encapsulamento.js
+++ 16-heranca-polimorfismo-encapsulamento.js
@@ -36,4 +36,24 @@ class Personagem {
   get mana() {
     return this.#mana;
   }
+  atacar(...args) {
+    if (args.length === 0) {
+      console.log(`${this.nome} REALIZA UM ataque normal!`);
+    } else if (args.length === 1) {
+      console.log(`${this.nome} atacar com um poder de ${args[0]}!`);
+    } else if (args.length === 2) {
+        console.log(`${this.nome} usou defesa ${args[1]} e atacou COM ${args[0]}de poder!`);
+      } else {
+        console.log("numero invalido de argumentos");
+}
+}
+
+defesa () {
+    console.log(`${this.nome} se defendeu com ${this.#nivel * 2} pontos de defesa `);
+
+}
+receberDano(dano) {
+    this.#vida -= dano;
+    console.log(`${this.nome} recebeu ${dano} de dano. Vida restante: ${this.#vida} `);
+}
 }
