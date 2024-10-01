5bea978d502b0d699f03cdf92b4d8f474d797811
João Victor <jv2404200717@gmail.com>
2024-10-01 07:43:21 -0400

    14-encapsulamento-atributos.js

---
+++ 14-encapsulamento-atributos.js
@@ -0,0 +1,16 @@
+class Personagem {
+
+#nome;
+#classe;
+#nivel;
+#vida;
+#mana;
+
+construtor(nome,classe,nivel,vida,mana) {
+    this.nome = nome;
+    this.classe = classe;
+    this.nivel = nivel;
+    this.vida = vida;
+    this.mana = mana;
+}
+}
