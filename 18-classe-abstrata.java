01e6f49c3263eba521b7e69b32853439f89ee97e
João Victor <jv2404200717@gmail.com>
2024-10-22 10:02:29 -0400

    add class gurreiro

--- 18-classe-abstrata.java
+++ 18-classe-abstrata.java
@@ -8,3 +8,22 @@ abstract class Personagem {
   public abstract void atacar ();
   public abstract void defender ();
   }
+
+  class Guerreiro extends Personagem {
+    private String arma ;
+
+    public Guerreiro(String nome, String arma) {
+        super(nome);
+        this.arma = arma;
+    }
+    
+    @Override
+    public void atacar() {
+        System.out.println(this.nome + " ataca com a " + this.arma+"!");
+    }
+
+    @Override
+    public void defender() {
+        System.out.println(this.nome + " levanta o escudo para se defender!");
+    }
+  }
